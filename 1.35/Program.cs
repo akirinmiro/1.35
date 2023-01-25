822357ae451f162a3bdb8088820270e42fe418e3
Сhert <hert@AKIRIN>
2023-01-25 13:01:03 +0300

    Добавьте файлы проекта.

---
+++ 1.35/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первый катет");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второй катет");
+        double b = Convert.ToDouble(Console.ReadLine());
+        double c = Math.Sqrt(a * a + b * b);
+        Console.WriteLine("Гипотенуза: {0}", c);
+    }
+}
+
